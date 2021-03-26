3bbd84bfca57832eb4017ecd014ef219782e791e
Funny Gustanty Aryndhy <funnyaryndhy@gmail.com>
2021-03-26 23:06:03 +0700

    Pengerjaan Tugas

---
+++ I0320042_Soal2_Tugas4.py
@@ -0,0 +1,4 @@
+angka_pertama = int(input("Masukkan angka pertama"))
+angka_kedua = int(input("Masukkan angka kedua"))
+
+print(angka_pertama//angka_kedua)
---
+++ I0320042_Soal3_Tugas4.py
@@ -0,0 +1,9 @@
+batas_berat = 50
+
+#A
+sapi = 115
+print(sapi <= batas_berat)
+
+#B
+lemari = 49
+print(lemari <= batas_berat)
